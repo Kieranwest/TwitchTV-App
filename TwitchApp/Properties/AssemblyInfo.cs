af3e371c877f3b00649e6dbd2bdbafb33b733830
Andrew Rylatt <andrew@rylatt.me>
2017-03-05 18:32:06 +0000

    Misread assembly fields

--- TwitchApp/Properties/AssemblyInfo.cs
+++ TwitchApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwitchApp")]
+[assembly: AssemblyTitle("TwitchTV-App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Westie1010")]
+[assembly: AssemblyCompany("Westie1010")]
+[assembly: AssemblyProduct("TwitchTV-App")]
 [assembly: AssemblyCopyright("Copyright © Westie1010 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
